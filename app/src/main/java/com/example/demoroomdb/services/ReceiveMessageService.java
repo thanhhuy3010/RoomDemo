9bc65b98de0efed5d06f945a708d08db72e0f29a
thanhhuy3010 <huyhuythanh3010@gmail.com>
2021-05-27 01:28:29 +0700

    update firebase

--- app/src/main/java/com/example/demoroomdb/services/ReceiveMessageService.java
+++ app/src/main/java/com/example/demoroomdb/services/ReceiveMessageService.java
@@ -3,22 +3,23 @@ package com.example.demoroomdb.services;
 import android.app.IntentService;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-public class ReceiveMessageService extends IntentService {
-    public ReceiveMessageService() {
-        super("Notification");
-    }
+import com.example.demoroomdb.model.Common.Logger.LoggerManager;
+import com.example.demoroomdb.model.Common.SharePreference.ConfigSharedPref;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
 
+public class ReceiveMessageService extends FirebaseMessagingService {
+    ConfigSharedPref sharedPref;
+    private static final String TAG = ReceiveMessageService.class.getSimpleName();
     @Override
-    protected void onHandleIntent(@Nullable Intent intent) {
+    public void onNewToken(@NonNull String token) {
+        Log.d(TAG, "Refreshed token: " + token);
 
-    }
-
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return super.onBind(intent);
     }
 }
